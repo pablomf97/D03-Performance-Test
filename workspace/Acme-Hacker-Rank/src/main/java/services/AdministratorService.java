9caa51b0eab79d2de28df268db9e5ff699ff43c8
pablomf97 <pablomf@hotmail.es>
2019-04-02 21:32:05 +0200

    Actor register en proceso

--- workspace/Acme-Hacker-Rank/src/main/java/services/AdministratorService.java
+++ workspace/Acme-Hacker-Rank/src/main/java/services/AdministratorService.java
@@ -1,11 +1,66 @@
 package services;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import repositories.AdministratorRepository;
+import security.Authority;
+import security.UserAccount;
+import domain.Administrator;
+import domain.CreditCard;
 
 @Transactional
 @Service
 public class AdministratorService {
 
+	/* Working repository */
+
+	@Autowired
+	private AdministratorRepository administratorRepository;
+
+	/* Simple CRUD methods */
+
+	public Administrator create() {
+		Administrator res;
+		UserAccount userAccount;
+		Authority auth;
+		Collection<Authority> authority;
+		CreditCard creditCard;
+
+		creditCard = new CreditCard();
+		auth = new Authority();
+		authority = new ArrayList<Authority>();
+		userAccount = new UserAccount();
+		res = new Administrator();
+
+		auth.setAuthority(Authority.ADMINISTRATOR);
+		authority.add(auth);
+		userAccount.setAuthorities(authority);
+
+		res.setUserAccount(userAccount);
+		res.setCreditCard(creditCard);
+
+		return res;
+	}
+
+	public Administrator findOne(Integer administratorId) {
+		Administrator res;
+
+		Assert.notNull(administratorId);
+		res = this.administratorRepository.findOne(administratorId);
+
+		return res;
+	}
+
+	public List<Administrator> findAll() {
+
+		return this.administratorRepository.findAll();
+	}
 }
--- workspace/Acme-Hacker-Rank/src/main/java/services/HackerService.java
+++ workspace/Acme-Hacker-Rank/src/main/java/services/HackerService.java
@@ -1,11 +1,71 @@
 package services;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import repositories.HackerRepository;
+import security.Authority;
+import security.UserAccount;
+import domain.CreditCard;
+import domain.Finder;
+import domain.Hacker;
 
 @Transactional
 @Service
 public class HackerService {
+
+	/* Working repository */
+
+	@Autowired
+	private HackerRepository hackerRepository;
+
+	/* Simple CRUD methods */
+
+	public Hacker create() {
+		Hacker res;
+		UserAccount userAccount;
+		Authority auth;
+		Collection<Authority> authority;
+		CreditCard creditCard;
+		Finder finder;
+
+		creditCard = new CreditCard();
+		auth = new Authority();
+		authority = new ArrayList<Authority>();
+		userAccount = new UserAccount();
+		finder = new Finder();
+		res = new Hacker();
+
+		auth.setAuthority(Authority.HACKER);
+		authority.add(auth);
+		userAccount.setAuthorities(authority);
+
+		res.setUserAccount(userAccount);
+		res.setCreditCard(creditCard);
+		res.setFinder(finder);
+
+		return res;
+	}
+
+	public Hacker findOne(Integer hackerId) {
+		Hacker res;
+
+		Assert.notNull(hackerId);
+		res = this.hackerRepository.findOne(hackerId);
+
+		return res;
+	}
+
+	public List<Hacker> findAll() {
+
+		return this.hackerRepository.findAll();
+	}
 
 }
