645c9e0b2761fbdf51a486b21bed32a3ff803887
cramersito <carlosalbalat97@gmail.com>
2019-04-23 12:35:06 +0200

    curricula

--- workspace/Acme-Hacker-Rank/src/main/java/services/CurriculaService.java
+++ workspace/Acme-Hacker-Rank/src/main/java/services/CurriculaService.java
@@ -1,11 +1,143 @@
 package services;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
+
+import repositories.CurriculaRepository;
+import domain.Curricula;
+import domain.EducationData;
+import domain.Hacker;
+import domain.PersonalData;
+import domain.PositionData;
 
 @Transactional
 @Service
 public class CurriculaService {
+
+	//Repository
+
+	@Autowired
+	private CurriculaRepository curriculaRepository;
+
+	//Services
+
+	@Autowired
+	private ActorService actorService;
+	
+	@Autowired
+	private PersonalDataService personalDataService;
+	
+	@Autowired
+	private Validator validator;
+
+	//Create
+	public Curricula create(){
+		Curricula result;
+		Hacker principal;
+
+		principal = (Hacker) this.actorService.findByPrincipal();
+		Assert.isTrue(this.actorService.checkAuthority(principal, "HACKER"));
+
+		result = new Curricula();
+		result.setEducationData(new ArrayList<EducationData>());
+		result.setPositionData(new ArrayList<PositionData>());
+		result.setPersonalData(this.personalDataService.defaultData());
+		result.setIsCopy(false);
+		result.setHacker(principal);
+		
+		this.curriculaRepository.saveAndFlush(result);
+		
+		return result;
+	}
+
+	//Save
+	public Curricula save(final Curricula curricula){
+		Curricula result;
+		Hacker principal;
+
+		//Checking curricula owner
+		principal = (Hacker) this.actorService.findByPrincipal();
+		Assert.isTrue(curricula.getHacker().getId() == principal.getId());
+
+		//Checking persistence
+		if(curricula.getId()==0){
+			Assert.notNull(curricula.getPersonalData());
+		}else{
+			Assert.notNull(curricula.getPersonalData());
+			Assert.notEmpty(curricula.getEducationData());
+			Assert.notEmpty(curricula.getPositionData());
+
+		}
+
+		result = this.curriculaRepository.save(curricula);
+
+		return result;
+	}
+
+	public void delete(Curricula curricula){
+		Hacker principal;
+
+		//Checking curricula owner
+		principal = (Hacker) this.actorService.findByPrincipal();
+		Assert.isTrue(curricula.getHacker().getId() == principal.getId());
+		
+		this.curriculaRepository.delete(curricula);
+		
+	}
+
+	//Finds
+	public Curricula findOne(int curriculaId){
+
+		Curricula result = this.curriculaRepository.findOne(curriculaId);
+
+		return result;
+	}
+
+	public Collection<Curricula> findAll(){
+
+		Collection<Curricula> result = this.curriculaRepository.findAll();
+
+		return result;
+	}
+	
+	public Collection<Curricula> getCurriculasByHacker(int hackerId){
+		
+		Collection<Curricula>result = this.curriculaRepository.getCurriculasByHacker(hackerId);
+		
+		return result;
+	}
+	
+	public Curricula getCurriculaByMiscellaneousData(int dataId){
+		Curricula result = this.curriculaRepository.getCurriculaByMiscellaneousData(dataId);
+		
+		return result;
+	}
+	
+	public Curricula getCurriculaByEducationData(int dataId){
+		Curricula result = this.curriculaRepository.getCurriculaByEducationData(dataId);
+		
+		return result;
+	}
+	
+	public Curricula getCurriculaByPositionData(int dataId){
+		Curricula result = this.curriculaRepository.getCurriculaByPositionData(dataId);
+		
+		return result;
+	}
+	
+	public Curricula getCurriculaByPersonalData(int dataId){
+		Curricula result = this.curriculaRepository.getCurriculaByPersonalData(dataId);
+		
+		return result;
+	}
+	
 
 }
--- workspace/Acme-Hacker-Rank/src/main/java/services/PersonalDataService.java
+++ workspace/Acme-Hacker-Rank/src/main/java/services/PersonalDataService.java
@@ -1,11 +1,127 @@
 package services;
 
+import java.util.Collection;
+
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import repositories.PersonalDataRepository;
+import domain.Curricula;
+import domain.Hacker;
+import domain.PersonalData;
 
 @Transactional
 @Service
 public class PersonalDataService {
+	
+	//Repository
+	@Autowired
+	private PersonalDataRepository personalDataRepository;
 
+	//Services
+	@Autowired
+	private ActorService actorService;
+
+	@Autowired
+	private CurriculaService curriculaService;
+
+	//Create
+	public PersonalData create(){
+		Hacker principal;
+		PersonalData result;
+		Collection<Curricula> principalCurriculas;
+
+		principal = (Hacker) this.actorService.findByPrincipal();
+
+		principalCurriculas = this.curriculaService.getCurriculasByHacker(principal.getId());
+
+		//Checking creating a data in an own curricula
+		for(Curricula c : principalCurriculas){
+			Assert.isTrue(c.getHacker().getId() == principal.getId());
+		}
+
+		result = new PersonalData();
+
+		return result;
+	}
+
+	//Save
+	public PersonalData save(PersonalData data,int curriculaId){
+		Hacker principal;
+		PersonalData dataDB = new PersonalData();
+		Collection<Curricula> principalCurriculas;
+		Curricula currentCurricula;
+		PersonalData result;
+
+		principal = (Hacker) this.actorService.findByPrincipal();
+
+		
+
+		principalCurriculas = this.curriculaService.getCurriculasByHacker(principal.getId());
+
+		
+
+		if(data.getId()!=0){
+			currentCurricula = this.curriculaService.getCurriculaByPositionData(data.getId());
+			
+			Assert.isTrue(currentCurricula.getPersonalData().getId() == data.getId());
+			Assert.isTrue(principalCurriculas.contains(currentCurricula));
+			Assert.isTrue(currentCurricula.getHacker().getId() == principal.getId());
+
+			dataDB = this.personalDataRepository.findOne(data.getId());
+
+			dataDB.setGithubProfile(data.getGithubProfile());
+			dataDB.setLinkedIn(data.getLinkedIn());
+			dataDB.setFullName(data.getFullName());
+			dataDB.setStatement(data.getStatement());
+			dataDB.setPhoneNumber(data.getPhoneNumber());
+
+
+			result = this.personalDataRepository.save(dataDB);
+
+		}else{
+			Assert.notNull(data.getGithubProfile());
+			Assert.notNull(data.getLinkedIn());
+			Assert.notNull(data.getFullName());
+			Assert.notNull(data.getStatement());
+			
+			result = this.personalDataRepository.save(data);
+			
+			currentCurricula = this.curriculaService.findOne(curriculaId);
+			
+			currentCurricula.setPersonalData(data);
+		}
+
+
+		return result;
+
+	}
+
+
+
+	//Finds
+	public PersonalData findOne(int dataId){
+		PersonalData result = this.personalDataRepository.findOne(dataId);
+
+		return result;
+	}
+
+	public Collection<PersonalData> findAll(){
+		Collection<PersonalData>result = this.personalDataRepository.findAll();
+
+		return result;
+	}
+	
+	public PersonalData defaultData(){
+		PersonalData result;
+		
+		result = new PersonalData();
+		result.setFullName("Its neccessary to introduce your full name");
+		result.setStatement("It is a must the statement");
+		
+		return result;
+	}
 }
